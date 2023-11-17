e9d320688ddf75f4e43c4ffeccc8e8dae36a5293
Tim Vahlbrock <tim@vahlbrock.de>
2023-11-17 14:45:22 +0100

    show basic location information in demo app

--- demo/src/App.tsx
+++ demo/src/App.tsx
@@ -1,7 +1,4 @@
-import { IonApp, IonRouterOutlet, setupIonicReact } from "@ionic/react";
-import { IonReactRouter } from "@ionic/react-router";
-import { Redirect, Route } from "react-router-dom";
-import Home from "./pages/Home";
+import { IonApp, IonContent, IonHeader, setupIonicReact } from "@ionic/react";
 
 /* Core CSS required for Ionic components to work properly */
 import "@ionic/react/css/core.css";
@@ -20,23 +17,83 @@ import "@ionic/react/css/text-alignment.css";
 import "@ionic/react/css/text-transformation.css";
 
 /* Theme variables */
+import { Geolocation, Position } from "@capacitor/geolocation";
+import { useEffect, useState } from "react";
 import "./theme/variables.css";
 
 setupIonicReact();
 
-const App: React.FC = () => (
-  <IonApp>
-    <IonReactRouter>
-      <IonRouterOutlet>
-        <Route exact path="/home">
-          <Home />
-        </Route>
-        <Route exact path="/">
-          <Redirect to="/home" />
-        </Route>
-      </IonRouterOutlet>
-    </IonReactRouter>
+const App: React.FC = () => {
+  const position = usePosition();
+
+  return (<IonApp>
+    <IonHeader>
+      <h1>Geolocation</h1>
+    </IonHeader>
+    <IonContent>
+      {position.isError && <p color="red">{position.error as string}</p>}
+      {!position.isError && (<p>
+        Latitude: {position.position.coords.latitude} < br />
+        Longitude: {position.position.coords.longitude}<br />
+        Accuracy: {position.position.coords.accuracy}<br />
+        Time: {position.position.timestamp}<br />
+      </p>)}
+    </IonContent>
   </IonApp>
-);
+  );
+};
+
+type PositionResult = PositionSuccessResult | PositionErrorResult;
+
+interface PositionSuccessResult {
+  isError: false;
+  position: Position;
+}
+
+interface PositionErrorResult {
+  isError: true;
+  error: unknown;
+}
+
+function usePosition(): PositionResult {
+  const [position, setPosition] = useState<PositionResult>({
+    isError: true,
+    error: "Waiting for Initial Position"
+  });
+
+  useEffect(() => {
+    Geolocation.watchPosition({
+      enableHighAccuracy: true,
+      timeout: 5000,
+      maximumAge: 5000
+    }, (position, err) => {
+      if (err) {
+        setPosition({
+          isError: true,
+          error: err
+        });
+        return;
+      }
+      if (!position) {
+        setPosition({
+          isError: true,
+          error: "No error, but still no position"
+        });
+        return;
+      }
+      setPosition({
+        isError: false,
+        position: position
+      });
+    }).catch(err => {
+      setPosition({
+        isError: true,
+        error: err
+      });
+    });
+  });
+
+  return position;
+}
 
 export default App;
