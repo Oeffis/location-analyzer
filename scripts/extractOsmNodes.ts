45841d55535e92fa6b3ba538803c068e1e6784d1
Tim Vahlbrock <tim@vahlbrock.de>
2023-11-22 21:28:43 +0100

    only extract notes that are present in ways

--- scripts/extractOsmNodes.ts
+++ scripts/extractOsmNodes.ts
@@ -1,4 +1,19 @@
-import { createReadStream, createWriteStream } from "fs";
+/* eslint-disable @typescript-eslint/no-unsafe-return */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+import { XMLParser } from "fast-xml-parser";
+import { createReadStream, createWriteStream, readFileSync } from "fs";
+
+const waysFile = readFileSync("../raw/osm-ways.xml", { encoding: "utf-8" });
+const xml = new XMLParser({ ignoreAttributes: false }).parse(waysFile);
+const nodeIds = xml.ways.way
+    .map((way: any) => way.nd.map((nd: any) => nd["@_ref"]))
+    .flat()
+    .map((id: string) => parseInt(id, 10));
+
+console.log("done parsing ways");
 
 const stream = createReadStream("../raw/no-git/nordrhein-westfalen-latest.osm", { encoding: "utf-8" });
 const writeStream = createWriteStream("../raw/osm-nodes.xml", { encoding: "utf-8" });
@@ -9,10 +24,13 @@ stream.on("data", (chunk) => {
     chunk = chunk.toString();
 
     const workingData = prevChunk + chunk;
-    const matches = workingData.matchAll(/<node([^>]*?\/>|.*?<\/node>)/gs);
+    const matches = workingData.matchAll(/<node id="(?<id>\d+)"([^>]*?\/>|.*?<\/node>)/gs);
     const matchArray = Array.from(matches);
 
-    const filteredMatchArray = matchArray.map((match) => match[0]);
+    const filteredMatchArray = matchArray
+        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+        .filter((match) => nodeIds.includes(parseInt(match.groups!.id, 10)))
+        .map((match) => match[0]);
 
     if (filteredMatchArray.length > 0) {
         writeStream.write(filteredMatchArray.join("\n  ") + "\n  ");
