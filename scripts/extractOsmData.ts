a96392152e14e5ecb936c8dfa1c41db53584a230
Tim Vahlbrock <tim@vahlbrock.de>
2023-12-21 18:22:46 +0100

    Scripts: Introduce OsmExtractorClass

--- scripts/extractOsmData.ts
+++ scripts/extractOsmData.ts
@@ -1,95 +1,102 @@
 import { createOSMStream } from "osm-pbf-parser-node";
 
-const waysToKeep = new Set<number>();
-const nodesToKeep = new Set<number>();
-const relations: Relation[] = [];
-const nodes: Node[] = [];
+class OsmExtractor {
+    private readonly waysToKeep = new Set<number>();
+    private readonly nodesToKeep = new Set<number>();
+    private readonly relations: Relation[] = [];
+    private readonly nodes: Node[] = [];
 
-const routeTypes = [
-    "bus",
-    "trolleybus",
-    "minibus",
-    "share_taxi",
-    "train",
-    "light_rail",
-    "subway",
-    "tram",
-    "monorail",
-    "ferry",
-    "funicular"
-];
-console.time("Relations");
-let stream = createOSMStream("../raw/no-git/Bochum.osm.pbf") as AsyncGenerator<OSMType, void>;
-for await (const item of stream) {
-    if (isRoot(item)) continue;
-    if (isRelation(item)) {
-        if (item.tags.type !== "route") continue;
-        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-        if (!routeTypes.includes(item.tags.route!)) continue;
-        relations.push(item);
-        const ways = item.members
-            .filter(member => member.type === "way"
-                && member.role === "")
-            .map(member => member.ref);
-        ways.forEach(way => waysToKeep.add(way));
-        continue;
+    private readonly routeTypes = [
+        "bus",
+        "trolleybus",
+        "minibus",
+        "share_taxi",
+        "train",
+        "light_rail",
+        "subway",
+        "tram",
+        "monorail",
+        "ferry",
+        "funicular"
+    ];
+
+    public async extract(): Promise<void> {
+        let stream = createOSMStream("../raw/no-git/Bochum.osm.pbf") as AsyncGenerator<OSMType, void>;
+        for await (const item of stream) {
+            if (isRoot(item)) continue;
+            if (isRelation(item)) {
+                if (item.tags.type !== "route") continue;
+                // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+                if (!this.routeTypes.includes(item.tags.route!)) continue;
+                this.relations.push(item);
+                const ways = item.members
+                    .filter(member => member.type === "way"
+                        && member.role === "")
+                    .map(member => member.ref);
+                ways.forEach(way => this.waysToKeep.add(way));
+                continue;
+            }
+        }
+        console.timeEnd("Relations");
+
+        console.time("Ways");
+        stream = createOSMStream("../raw/no-git/Bochum.osm.pbf") as AsyncGenerator<OSMType, void>;
+        for await (const item of stream) {
+            if (isRoot(item)) continue;
+            if (isWay(item)) {
+                if (!this.waysToKeep.has(item.id)) continue;
+                const nodes = item.refs ?? [];
+                nodes.forEach(node => this.nodesToKeep.add(node));
+                continue;
+            }
+        }
+        console.timeEnd("Ways");
+
+        console.time("Nodes");
+        stream = createOSMStream("../raw/no-git/Bochum.osm.pbf") as AsyncGenerator<OSMType, void>;
+        for await (const item of stream) {
+            if (isRoot(item)) continue;
+            if (isNode(item)) {
+                if (!this.nodesToKeep.has(item.id)) continue;
+                this.nodes.push(item);
+                continue;
+            }
+        }
+        console.timeEnd("Nodes");
+
+        console.log("Done filtering, checking if we have everything");
+
+        const missing = Array.from(this.nodesToKeep.values()).filter(node => !this.nodes.find(n => n.id === node));
+        if (missing.length > 0) {
+            console.log("Missing nodes", missing);
+        } else {
+            console.log(`All nodes onboard (${this.nodes.length} for ${this.relations.length} routes)`);
+        }
+
+        function isRoot(item: OSMType): item is Root {
+            // eslint-disable-next-line no-prototype-builtins
+            return !item.hasOwnProperty("type");
+        }
+
+        function isNode(item: OSMType): item is Node {
+            return (item as OSMNonRootType).type === "node";
+        }
+
+        function isWay(item: OSMType): item is Way {
+            return (item as OSMNonRootType).type === "way";
+        }
+
+        function isRelation(item: OSMType): item is Relation {
+            return (item as OSMNonRootType).type === "relation";
+        }
     }
 }
-console.timeEnd("Relations");
 
-console.time("Ways");
-stream = createOSMStream("../raw/no-git/Bochum.osm.pbf") as AsyncGenerator<OSMType, void>;
-for await (const item of stream) {
-    if (isRoot(item)) continue;
-    if (isWay(item)) {
-        if (!waysToKeep.has(item.id)) continue;
-        const nodes = item.refs ?? [];
-        nodes.forEach(node => nodesToKeep.add(node));
-        continue;
-    }
-}
-console.timeEnd("Ways");
-
-console.time("Nodes");
-stream = createOSMStream("../raw/no-git/Bochum.osm.pbf") as AsyncGenerator<OSMType, void>;
-for await (const item of stream) {
-    if (isRoot(item)) continue;
-    if (isNode(item)) {
-        if (!nodesToKeep.has(item.id)) continue;
-        nodes.push(item);
-        continue;
-    }
-}
-console.timeEnd("Nodes");
-
-console.log("Done filtering, checking if we have everything");
-
-const missing = Array.from(nodesToKeep.values()).filter(node => !nodes.find(n => n.id === node));
-if (missing.length > 0) {
-    console.log("Missing nodes", missing);
-} else {
-    console.log(`All nodes onboard (${nodes.length} for ${relations.length} routes)`);
-}
+const extractor = new OsmExtractor();
+extractor.extract().catch(console.error);
 
 type OSMType = Root | OSMNonRootType;
 type OSMNonRootType = Node | Way | Relation;
-
-function isRoot(item: OSMType): item is Root {
-    // eslint-disable-next-line no-prototype-builtins
-    return !item.hasOwnProperty("type");
-}
-
-function isNode(item: OSMType): item is Node {
-    return (item as OSMNonRootType).type === "node";
-}
-
-function isWay(item: OSMType): item is Way {
-    return (item as OSMNonRootType).type === "way";
-}
-
-function isRelation(item: OSMType): item is Relation {
-    return (item as OSMNonRootType).type === "relation";
-}
 
 interface Root {
     source: string;
